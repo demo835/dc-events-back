9ace3441f603a4ae9ee377a5ae132e1491961901
Thomas Ferullo <thomasferullo@Andrews-MacBook-Pro.local>
2018-08-20 16:51:30 -0400

    got full crud in backend

--- db/models/Event.js
+++ db/models/Event.js
@@ -2,17 +2,17 @@ const mongoose = require("../connection");
 const Schema = mongoose.Schema;
 const Venue = require("./Venue");
 
-const Event = new mongoose.Schema({
-  name: String,
-  // venue: Venue,
-  venue: {
-    type: Schema.Types.ObjectId,
-    ref: "Venue"
+const Event = new mongoose.Schema(
+  {
+    name: String,
+    // venue: Venue,
+    venue: String,
+    description: String,
+    food: String,
+    cost: Number
   },
-  description: String,
-  food: String,
-  cost: Number
-});
+  { collection: "event" }
+);
 
 module.exports = mongoose.model("Event", Event);
 
