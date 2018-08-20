c5aca3fd8dc77d2067ff8cba303a5cc55ea0046d
Thomas Ferullo <thomasferullo@Andrews-MacBook-Pro.local>
2018-08-20 13:53:20 -0400

    updated index

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const parser = require('body-parser')
+const express = require("express");
+const parser = require("body-parser");
 // const mongoose = require('./db/schema.js')
 
-const app = express()
+const app = express();
 
-app.set('port', process.env.PORT || 3001)
-app.use(parser.json())
+app.set("port", process.env.PORT || 3001);
+app.use(parser.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(app.get('port'), () => {
-    console.log("Locked and loaded on " + app.get('port'))
-})
+app.listen(app.get("port"), () => {
+  console.log("Locked and loaded on " + app.get("port"));
+});
