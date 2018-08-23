f751ee773c603f858cc7ce4239b301db156ee3e9
demo835 <demo835@yahoo.com>
2018-08-23 12:06:08 -0400

    ready for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/dc-events");
+// mongoose.connect("mongodb://localhost/dc-events");
+
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect("mongodb://localhost/dc-events");
+  }
 
 mongoose.Promise = Promise;
 
